b8541a23cf386ffcd41b9a96c3a276a5876da161
aaron weiss <aaronweiss@aarons-MacBook-Air.local>
2024-09-06 06:21:05 -0400

    Remove environment files from version control and add to .gitignore

--- config/config.js
+++ config/config.js
@@ -1,18 +1,22 @@
-require('dotenv').config();
+const env = process.env.NODE_ENV || 'development';  // Default to development if NODE_ENV isn't set
+const machine = process.env.MACHINE_ENV || 'home';  // Default to home if MACHINE_ENV isn't set
+
+// Load the appropriate .env file based on NODE_ENV and MACHINE_ENV
+require('dotenv').config({ path: `.env.${env}.${machine}` });
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'aaronweiss',  // Using 'aaronweiss' as the default if DB_USER is not set
-    password: process.env.DB_PASSWORD || 'qwerty',  // Using 'qwerty' as the default if DB_PASSWORD is not set
-    database: process.env.DB_NAME || 'bud_db_dev',  // Defaulting to 'bud_db_dev' if DB_NAME is not set
-    host: process.env.DB_HOST || 'localhost',  // Defaulting to 'localhost' if DB_HOST is not set
-    port: process.env.DB_PORT || 5432,  // Defaulting to port 5432 if DB_PORT is not set
+    username: process.env.DB_USER || 'aaronweiss',
+    password: process.env.DB_PASSWORD || 'qwerty',
+    database: process.env.DB_NAME || 'bud_db_dev',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
   },
   production: {
-    username: process.env.DB_USER || 'aaronweiss',
-    password: process.env.DB_PASSWORD || 'qwerty',
-    database: process.env.DB_NAME || 'bud_db_prod',  // Assuming you have a production database
+    username: process.env.DB_USER || 'prod_user',
+    password: process.env.DB_PASSWORD || 'secure_password',
+    database: process.env.DB_NAME || 'bud_db_prod',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
